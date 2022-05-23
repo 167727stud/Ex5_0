55b886cd9691bdeebf2bc0fb672092606441eccd
167727 <100698238+167727stud@users.noreply.github.com>
2022-05-23 18:08:05 +0000

    Commit 5_4

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
   const cw1 = document.getElementById('cw1')
   const cw2 = document.getElementById('cw2')
   const cw3 = document.getElementById('cw3')
+  const cw4 = document.getElementById('cw4')
   const answer = document.getElementById('answer')
 
   example.addEventListener("click", function () {
@@ -18,11 +19,11 @@
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
       .then(array => {
-        console.log(array)
+        //console.log(array)
         answer.innerHTML = "  "
         array.forEach(post => answer.innerHTML+=
           `
-  ${post.userId}
+  ${post.title}
           <hr/>
           `
           
@@ -36,16 +37,16 @@
     //TODO implement it
     answer.innerHTML = "Loading ......."
     answer.innerHTML = "Loading ........."
-    answer.innerHTML = "Loading .............."
-     answer.innerHTML = "Loading ...................."
-     fetch('https://jsonplaceholder.typicode.com/posts')
+    answer.innerHTML = "Loading ............."
+    answer.innerHTML = "Loading ...................."
+    fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
       .then(array => {
-        console.log(array)
+        //console.log(array)
         answer.innerHTML = "  "
         array.forEach(post => answer.innerHTML+=
           `
-  ${post.userId}
+  ${post.title}
           <hr/>
           `
           
@@ -56,6 +57,35 @@
 
   cw3.addEventListener("click", function () {
     //TODO implement it
+    answer.innerHTML = "Loading ...................."
+    fetch('https://jsonplaceholder.typicode.com/posts/7')
+      .then(response => response.json())
+      .then(post => {
+        //console.log(post)
+        answer.innerHTML = post.title
+        answer.innerHTML += '<hr/>' + post.body
+      })
+    
+
+  })
+
+  cw4.addEventListener("click", function () {
+    //TODO implement it
+    answer.innerHTML = "Processing ...................."
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'nowy',
+    body: 'nowynowynowynowynowynowynowynowynowynowynowynowy',
+    userId: 7,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+  })
+  .then((response) => response.json())
+  .then((json) => answer.innerHTML = 'Dodano nowy post o ID: ' + json.id);
+
   })
 
 })();
